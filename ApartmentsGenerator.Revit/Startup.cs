4fe9cbf50ed55887bb91492b286c0d05ac9eb8db
savelev.g <savelev.g@skbkontur.ru>
2024-11-07 16:58:30 +0500

    Initial commit

---
+++ ApartmentsGenerator.Revit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace ApartmentsGenerator.Revit;
+
+[Transaction(TransactionMode.Manual)]
+public class Startup : IExternalCommand
+{
+    public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+    {
+        throw new NotImplementedException();
+    }
+}
